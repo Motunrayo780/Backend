35f78ee6185622f21f876644e9feb3b0fa4f4be9
Motunrayo780 <bakaremotunrayo818@gmail.com>
2025-04-02 05:26:16 -0700

    commit 2

--- router/userRouter.js
+++ router/userRouter.js
@@ -5,7 +5,7 @@ import {getUser, postUser,getSingleUser,deleteUser  } from '../controller/userCo
 
 const router= Router()
 
-router.get( "/user/", getUser )
+router.get("/user/", getUser )
 router.post("/users/", postUser)
 router.get("/user/:id", getSingleUser )
 router.delete("/user/:id", deleteUser )
